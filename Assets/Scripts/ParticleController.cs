bbd951b1b438b9d3dd5a1eaa7657de6cf1094d79
brenberg <75150191+brenberg@users.noreply.github.com>
2021-03-22 16:01:54 -0400

    design doc update

--- Assets/Scripts/ParticleController.cs
+++ Assets/Scripts/ParticleController.cs
@@ -16,7 +16,7 @@ public class ParticleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.GetKey(KeyCode.Space))
         {
             shootPart.Play();
         }
