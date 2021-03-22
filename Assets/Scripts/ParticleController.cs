dce4ca70bc3cba5610aaeac00e86d2308361e895
brenberg <75150191+brenberg@users.noreply.github.com>
2021-03-21 23:04:05 -0400

    Tried to do paint stuff, but meh

---
+++ Assets/Scripts/ParticleController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    public ParticleSystem shootPart;
+    public GameObject brush;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shootPart.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            shootPart.Play();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            shootPart.Stop();
+        }
+
+        if (shootPart.IsAlive(false))
+        {
+            //Debug.Log("DEAD");
+
+            //var Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //RaycastHit hit;
+            //if (Physics.Raycast(Ray, out hit))
+            //{
+            //    var go = Instantiate(brush, hit.point + Vector3.up * 0.1f, Quaternion.identity, transform);
+            //    //go.transform.localScale = Vector3.one * BrushSize;
+            //}
+        }
+
+        if (shootPart.IsAlive(true))
+        {
+            //Debug.Log("ALIVE");
+        }
+    }
+}
---
+++ Assets/Scripts/ParticleDeath.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDeath : MonoBehaviour
+{
+    public float radius = 5f;
+    public float power = 100f;
+    public float liftPower = 50f;
+    private ParticleSystem PSystem;
+    private ParticleCollisionEvent[] CollisionEvents;
+
+    void Start()
+    {
+        PSystem = GetComponent<ParticleSystem>();
+}
+
+    public void OnParticleCollision(GameObject other)
+    {
+        int collCount = PSystem.GetSafeCollisionEventSize();
+
+        if (collCount > CollisionEvents.Length)
+        {
+            CollisionEvents = new ParticleCollisionEvent[collCount];
+        }
+        int eventCount = PSystem.GetCollisionEvents(other, CollisionEvents);
+
+        for (int i = 0; i < eventCount; i++)
+        {
+            Explosions(CollisionEvents[i].intersection);
+
+            //TODO: Do your collision stuff here. 
+            // You can access the CollisionEvent[i] to obtaion point of intersection, normals that kind of thing
+            // You can simply use "other" GameObject to access it's rigidbody to apply force, or check if it implements a class that takes damage or whatever
+        }
+    }
+
+    void Explosions(Vector3 intersection)
+    {
+
+        Vector3 explosionPos = intersection;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders)
+        {
+            if (hit && hit.GetComponent<Rigidbody>())
+            {
+                hit.GetComponent<Rigidbody>().AddExplosionForce(power, explosionPos, radius, liftPower);
+            }
+        }
+
+        GetComponent<AudioSource>().Play();
+
+    }
+}
