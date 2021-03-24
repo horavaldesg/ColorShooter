09710a8cd4b2235db5749a717d6dff370dec5c8d
Horacio Valdes <horavaldesg@gmail.com>
2021-03-23 22:41:10 -0400

    Paint

--- Assets/Scripts/ColorChecker.cs
+++ Assets/Scripts/ColorChecker.cs
@@ -17,7 +17,35 @@ public class ColorChecker : MonoBehaviour
     }
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        //Debug.Log(hit.gameObject.GetComponent<Renderer>().material.color);
-        
+        if (hit.collider.gameObject.GetComponent<Renderer>().material.color != null &&
+            hit.collider.gameObject.name != "XLeft" &&
+            hit.collider.gameObject.name != "XRight" &&
+            hit.collider.gameObject.name != "ZLeft" &&
+            hit.collider.gameObject.name != "ZRight" &&
+            hit.collider.gameObject.name != "YTop" &&
+            hit.collider.gameObject.name != "YBottom")
+            Debug.Log(hit.collider.name);
+        {
+            if (hit.collider.gameObject.GetComponent<Renderer>().material.color == Color.magenta)
+            {
+                Debug.Log("Magenta");
+            }
+            else if (hit.collider.gameObject.GetComponent<Renderer>().material.color == Color.cyan)
+            {
+                Debug.Log("Cyan");
+            }
+            else if (hit.collider.gameObject.GetComponent<Renderer>().material.color == Color.yellow)
+            {
+                Debug.Log("Yellow");
+            }
+            else if (hit.collider.gameObject.GetComponent<Renderer>().material.color == Color.black)
+            {
+                Debug.Log("Black");
+            }
+
+            Debug.Log(hit.gameObject.GetComponent<Renderer>().material.color);
+
+        }
+
     }
 }
--- Assets/Scripts/UVCopyScript.cs
+++ Assets/Scripts/UVCopyScript.cs
@@ -5,17 +5,41 @@ using UnityEngine;
 public class UVCopyScript : MonoBehaviour
 {
     public GameObject brush;
+    [SerializeField]Material splatter;
     public float BrushSize = 0.1f;
-
+    Color color;
     // Start is called before the first frame update
     void Start()
     {
-        
+        color = Color.magenta;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            color = Color.magenta;
+            //splatter.color = color;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            color = Color.cyan;
+            //splatter.color = color;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            color = Color.yellow;
+            //splatter.color = color;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            color = Color.black;
+            //splatter.color = color;
+        }
+        
         if (Input.GetMouseButton(0))
         {
             var Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -27,39 +51,47 @@ public class UVCopyScript : MonoBehaviour
                     if (hit.collider.gameObject.name == "XLeft")
                     {
                         Debug.Log("X Left");
-
+                        
                         var go = Instantiate(brush, hit.point + Vector3.up * 0.1f, Quaternion.Euler(90, 0, 0), transform);
+                        go.GetComponent<Renderer>().material.color = color;
+
                     }
                     else if(hit.collider.gameObject.name == "XRight")
                     {
                         var go = Instantiate(brush, hit.point + Vector3.up * 0.1f, Quaternion.Euler(-90, 0, 0), transform);
+                        go.GetComponent<Renderer>().material.color = color;
 
                         Debug.Log("X Right");
                     }
                     else if(hit.collider.gameObject.name == "ZLeft")
                     {
                         var go = Instantiate(brush, hit.point + Vector3.up * 0.1f, Quaternion.Euler(0, 0, 90), transform);
+                        go.GetComponent<Renderer>().material.color = color;
 
                         Debug.Log("Z Left");
                     }
                     else if(hit.collider.gameObject.name == "ZRight")
                     {
                         var go = Instantiate(brush, hit.point + Vector3.up * 0.1f, Quaternion.Euler(0, 0, -90), transform);
+                        go.GetComponent<Renderer>().material.color = color;
                         Debug.Log("Z Right");
                     }
                     else if(hit.collider.gameObject.name == "YTop")
                     {
                         var go = Instantiate(brush, hit.point + Vector3.up * 0.1f, Quaternion.Euler(0, 0, 0), transform);
+                        go.GetComponent<Renderer>().material.color = color;
                         Debug.Log("Y Top");
                     }
                     else if(hit.collider.gameObject.name == "YBottom")
                     {
                         var go = Instantiate(brush, hit.point + Vector3.up * 0.1f, Quaternion.Euler(180, 0, 0), transform);
+                        go.GetComponent<Renderer>().material.color = color;
                         Debug.Log("Y Bottom");
                     }
                     else
                     {
                         var go = Instantiate(brush, hit.point + Vector3.up * 0.1f, Quaternion.Euler(0, 0, 0), transform);
+                        go.GetComponent<Renderer>().material.color = color;
 
                     }
                 }
