97f0efbec7b1c248aaf40dae49bb470c515c4c52
brenberg <75150191+brenberg@users.noreply.github.com>
2021-03-29 13:02:44 -0400

    level complete

---
+++ Assets/Scripts/ColorFlux.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorFlux : MonoBehaviour
+{
+    public Material RainbowMat;
+    private MeshRenderer mr;
+
+    public float rainbowSpeed;
+    float hue;
+    float sat;
+    float bri;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color.RGBToHSV(mr.material.color, out hue, out sat, out bri);
+        hue += rainbowSpeed / 10000;
+        if(hue >= 1)
+        {
+            hue = 0;
+        }
+        sat = 1;
+        bri = 1;
+        mr.material.color = Color.HSVToRGB(hue, sat, bri);
+    }
+}
