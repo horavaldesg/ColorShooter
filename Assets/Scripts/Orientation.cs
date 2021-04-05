3542be8b14a4cf4fdfa76bc3815fd3de0d10a6c9
Horacio Valdes <horavaldesg@gmail.com>
2021-04-05 00:08:01 -0400

    Orientation Cube

---
+++ Assets/Scripts/Orientation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orientation : MonoBehaviour
+{
+    public float sens = 50;
+    public Transform camTransform;
+    float camRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!moveCharacter.gravityChange)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
+            //camRotation += mouseY;
+            transform.Rotate(new Vector3(mouseY, mouseX, 0));
+
+        }
+        else
+        {
+            float mouseX = -Input.GetAxis("Mouse X") * sens * Time.deltaTime;
+            transform.Rotate(new Vector3(transform.rotation.x, mouseX, transform.rotation.z));
+            float mouseY = -Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
+            camRotation += mouseY;
+            camRotation = Mathf.Clamp(camRotation, -80f, 90f);
+            camTransform.localRotation = Quaternion.Euler(new Vector3(camRotation, transform.rotation.y, transform.rotation.z));
+        }
+    }
+}
